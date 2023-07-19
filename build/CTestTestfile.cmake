3b11cc4e1c5bc2d78ce08e47b0b8214df5635f7a
Aditi <avshinde_b21@el.vjti.ac.in>
2023-07-19 17:26:19 +0530

    my first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/catastrophe/arm_ws/src
+# Build directory: /home/catastrophe/arm_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("gazebo_ros_demos/rrbot_control")
+subdirs("gazebo_ros_demos/rrbot_description")
+subdirs("gazebo_ros_demos/rrbot_gazebo")
+subdirs("arm_sim")
+subdirs("myrobo_gazebo")
+subdirs("gazebo_ros_demos/gazebo_tutorials")
---
+++ build/myrobo_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/catastrophe/arm_ws/src/myrobo_gazebo
+# Build directory: /home/catastrophe/arm_ws/build/myrobo_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
